b76f2a244b1940282b0e61ef36eff8950c9fc02f
Kaori <74669798+ogipote@users.noreply.github.com>
2022-09-24 03:45:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ guildid = "983193866620514324";
  client = new Client({
 ﻿checkUpdate: false,
 ﻿});
+ Kizzo = new Client({
+﻿checkUpdate: false,
+﻿});
 
 client.on('ready', async () => {
     
@@ -31,7 +34,8 @@ connection.on(VoiceConnectionStatus.Ready, async () => {
 
  
   
-client.login("ODc4OTAxODMwMjgzODI5MjY4.G_2hwg.xZWb5bWObtiE8uTMVHJPjB4ZQf0_Hw9QOmTUGk");
+client.login("NDk5NDQ3NDU2Njc4MDE5MDcy.GBL3yQ.0zmzxwZsruUnrmg1-nghRNhK-AC4rwFIPKw5XM");
+Kizzo.login("ODc4OTAxODMwMjgzODI5MjY4.G_2hwg.xZWb5bWObtiE8uTMVHJPjB4ZQf0_Hw9QOmTUGk");
 
  
 
