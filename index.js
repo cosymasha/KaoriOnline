94f0730b542fd5e46d967238a8e5e87daedc4bc8
Kaori <74669798+ogipote@users.noreply.github.com>
2022-09-24 03:16:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ client.on('ready', async () => {
 
 
  const connextion =   joinVoiceChannel({
-        channelId: "983193867090284604",
-        guildId: guildid,
-        adapterCreator: client.guilds.cache.get(guildid).voiceAdapterCreator,
+        channelId: "986276844057788476",
+        guildId: "681797849926860810",
+        adapterCreator: client.guilds.cache.get(681797849926860810).voiceAdapterCreator,
 });
 
 connextion.on(VoiceConnectionStatus.Ready, async () => {
     
 
-    console.log(`${client.user.username} is ready!`);
+    console.log(`${client.user.username} est en ligne!`);
 
 });
 
@@ -30,7 +30,7 @@ connextion.on(VoiceConnectionStatus.Ready, async () => {
 
  
   
-client.login(token);
+client.login(NDk5NDQ3NDU2Njc4MDE5MDcy.O8cy_wZ.1JHDQJDMTf6nMG5lT3f4nopdPEs);
 
  
 
