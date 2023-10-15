783eee51ecb7f72fc3d1c6bd02ac313d1b03e393
Masha <97138114+cosymasha@users.noreply.github.com>
2023-10-15 17:24:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ guildid = "681797849926860810";
  client1 = new Client({
 ﻿checkUpdate: false,
 ﻿});
- client2 = new Client({
-﻿checkUpdate: false,
-﻿});
+ //client2 = new Client({
+//﻿checkUpdate: false,
+//﻿});
  // client3 = new Client({
 // ﻿checkUpdate: false,
 ﻿// });
- client4 = new Client({
-﻿checkUpdate: false,
-﻿});
-client5 = new Client({
-﻿checkUpdate: false,
-﻿});
+// client4 = new Client({
+//﻿checkUpdate: false,
+//﻿});
+//client5 = new Client({
+//﻿checkUpdate: false,
+//﻿});
 client6 = new Client({
 ﻿checkUpdate: false,
 ﻿});
@@ -61,9 +61,9 @@ connection.on(VoiceConnectionStatus.Ready, async () => {
   
 client.login(process.env.Rina);
 client1.login(process.env.Masha);
-client2.login(process.env.Olaf);
-client4.login(process.env.Ronin);
-client5.login(process.env.Himeji);
+// client2.login(process.env.Olaf);
+// client4.login(process.env.Ronin);
+// client5.login(process.env.Himeji);
 client6.login(process.env.Keii);
 client7.login(process.env.Frost);
 client8.login(process.env.Rosie);
