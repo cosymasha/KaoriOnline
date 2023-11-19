4c285a8d1091246965aa68e51fce38b227677873
Masha <97138114+cosymasha@users.noreply.github.com>
2023-11-19 14:44:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,67 +5,77 @@ const { Client, Channel } = require('discord.js-selfbot-v13');
 guildid = "681797849926860810";
 
  client = new Client({
-﻿checkUpdate: false,
-﻿});
+checkUpdate: false,
+shards: 1,
+});
  client1 = new Client({
- ﻿checkUpdate: false,
+ checkUpdate: false,
+ shards: 1,
 });
  client2 = new Client({
- ﻿checkUpdate: false,
+ checkUpdate: false,
+ shards: 1,
 });
  client3 = new Client({
- ﻿checkUpdate: false,
+ checkUpdate: false,
+ shards: 1,
 });
- //client4 = new Client({
- //checkUpdate: false,
-//});
+	client4 = new Client({
+	checkUpdate: false,
+	shards: 1,
+	});
  client5 = new Client({
 checkUpdate: false,
+shards: 1,
 });
 client6 = new Client({
-﻿checkUpdate: false,
-﻿});
+checkUpdate: false,
+shards: 1,
+});
 client7 = new Client({
-﻿checkUpdate: false,
-﻿});
+checkUpdate: false,
+shards: 1,
+});
 client8 = new Client({
-﻿checkUpdate: false,
-﻿});
+checkUpdate: false,
+shards: 1,
+});
 client9 = new Client({
-﻿checkUpdate: false,
-﻿});
+checkUpdate: false,
+shards: 1,
+});
 
 client.on('ready', async () => {
-    
+
 
 
  const connection =   joinVoiceChannel({
-        channelId: "986276840400355349",
-        guildId: "681797849926860810",
-        adapterCreator: client.guilds.cache.get(guildid).voiceAdapterCreator,
-        selfDeaf: false,
+				channelId: "986276840400355349",
+				guildId: "681797849926860810",
+				adapterCreator: client.guilds.cache.get(guildid).voiceAdapterCreator,
+				selfDeaf: false,
 });
 
 connection.on(VoiceConnectionStatus.Ready, async () => {
-    
 
-    console.log(`${client.user.username} est en ligne!`);
+
+		console.log(`${client.user.username} est en ligne!`);
 
 });
 
-    });
+		});
 
 
 
- 
-  
-client.login(process.env.Rina);
-client1.login(process.env.Masha);
-client2.login(process.env.Lea);
-client3.login(process.env.Sumire);
-// client4.login(process.env.Ronin);
-client5.login(process.env.Himeji);
-client6.login(process.env.Keii);
-client7.login(process.env.Frost);
-client8.login(process.env.Rosie);
-client9.login(process.env.Cassidy);
+
+
+		client.login(process.env.RINA);
+		client1.login(process.env.MASHA);
+		client2.login(process.env.LEA);
+		client3.login(process.env.SHAYLA);
+		client4.login(process.env.SMOKIE);
+		client5.login(process.env.YUKI);
+		client6.login(process.env.KEII);
+		client7.login(process.env.FROST);
+		client8.login(process.env.KREAM);
+		client9.login(process.env.CASSIDY);
